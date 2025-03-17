0b5a8dd815fb9340848dd79f3c161a7bc62125c7
Maksym Drachenko <maks7d@gmail.com>
2025-03-17 15:08:23 +0500

    init

---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,11 @@
+export const Description = () => {
+  return (
+    <>
+      <h1 className="title">Sip Happens Café</h1>
+      <p className="subtitle">
+        Please leave your feedback about our service by selecting one of the
+        options below.
+      </p>
+    </>
+  );
+};
